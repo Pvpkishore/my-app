2eef727f78442cbbfe4c89f31ac257db938f746f
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2024-12-29 20:43:35 +0530

    Update page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -50,7 +50,7 @@ const GenerateComponent = () => {
       redirect: "follow",
     };
 
-    const r = await fetch("http://localhost:3000/api/add", requestOptions);
+    const r = await fetch("https://linktrekishore.vercel.app/api/add", requestOptions);
     const result = await r.json();
     if (result.success) {
       toast.success(result.message);
