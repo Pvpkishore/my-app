8735ff8a5e9b11f2098fa70f2190e30baa94b743
pvpkishore <pvpkishore09@gmail.com>
2024-12-29 12:23:33 +0530

    LInktree

--- app/page.js
+++ app/page.js
@@ -1,101 +1,72 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const router = useRouter()
+  const [text, setText] = useState("")
+
+  const createTree = () => {
+    router.push(`/generate?handle=${text}`)
+  }
+
+  return (
+    <main>
+      <section className="bg-[#254f1a] pt-40 min-h-[100vh] grid grid-cols-2 tracking-tighter font-poppins">
+        <div className="flex justify-center flex-col ml-[8vw] gap-3">
+          <p className="text-[#d2e823] font-extrabold text-6xl">Everything you </p>
+          <p className="text-[#d2e823] font-extrabold text-6xl">are. In one,</p>
+          <p className="text-[#d2e823] font-extrabold text-6xl">simple link in bio.</p>
+          <p className="text-[#d2e823] text-xl my-4">Join 50M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+          <div className="input flex gap-2">
+            <input value={text} onChange={(e) => setText(e.target.value)} className="px-2  text-gray-900 py-2 focus:outline-green-800 rounded-md" type="text" placeholder="Enter your Handle" />
+            <button onClick={() => createTree()} className="bg-pink-300 tracking-normal font-poppins text-black rounded-full px-4 py-4 font-semibold hover:bg-pink-400 ">Claim your Bittree</button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex items-center justify-center flex-col ml-[4vw]">
+          <img src="/home.png" alt="homepage image" />
+        </div>
+      </section>
+
+
+      <section className="bg-[#e9c0e9] min-h-[100vh] grid grid-cols-2 font-poppins">
+        <div className="flex items-center justify-center flex-col ml-[4vw]">
+          <img className="h-2/3" src="./secondpage.png" alt="homepage image" />
+        </div>
+        <div className="flex justify-center flex-col ml-[3vw] gap-3 tracking-tighter">
+          <p className="text-purple-900 font-extrabold text-5xl">Create and customize</p>
+          <p className="text-purple-900 font-extrabold text-5xl">your Linktree in minutes</p>
+          <p className="text-purple-900 text-xl my-4">Connect your TikTok, Instagram, Twitter, website, store, videos, music, podcast, events and more. It all comes together in a link in bio landing page designed to convert.</p>
+          <div className="flex tracking-normal">
+          <button className="bg-purple-900 text-white rounded-full px-4 py-4 font-semibold hover:bg-purple-800">Get started for free</button>
+        </div>
+        </div>
+        
+      </section>
+
+      <section className="bg-[#780016] min-h-[100vh]  grid grid-cols-2 font-poppins">
+        <div className="flex justify-center flex-col ml-[6vw] gap-3 tracking-tighter">
+          <p className="text-[#e9c0e9] font-extrabold text-5xl text-nowrap">Share your Linktree from</p>
+          <p className="text-[#e9c0e9] font-extrabold text-5xl">your Instagram, TikTok,</p>
+          <p className="text-[#e9c0e9] font-extrabold text-5xl">Twitter and other bios</p>
+          <p className="text-[#e9c0e9] text-xl my-4">Add your unique Linktree URL to all the platforms and places you find your audience. Then use your QR code to drive your offline traffic online.</p>
+          <div className="flex tracking-normal">
+          <button className="bg-[#e9c0e9] text-black rounded-full px-4 py-4 font-semibold hover:bg-[#f07af0]">Get started for free</button>
+        </div>
+        </div>
+        <div className="flex items-center justify-center flex-col ml-[4vw]">
+          <img className="h-2/3" src="./Thirdpage.png" alt="homepage image" />
+        </div>
+      </section>
+
+      <section className="bg-[#502274] min-h-[60vh]">
+              <div className="p-20">
+                <img className="h-3/6  rounded-3xl" src="./linktre.png" alt="homepage image" />
+              </div>
+      </section>
+    </main>
   );
 }
